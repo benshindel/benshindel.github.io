c93edbab667ff39e8ad2ce9423b229ea0b0ce929
benshindel <65996825+benshindel@users.noreply.github.com>
2024-12-23 16:39:31 -0500

    Create menu-toggle.js

---
+++ assets/js/menu-toggle.js
@@ -0,0 +1,11 @@
+// Toggle Mobile Menu
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menu-toggle");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  if (menuToggle && mobileMenu) {
+    menuToggle.addEventListener("click", () => {
+      mobileMenu.classList.toggle("active");
+    });
+  }
+});
