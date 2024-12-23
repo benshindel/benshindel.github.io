15891e2a205a1d1f3dcc523e37f762ec626a37c0
benshindel <65996825+benshindel@users.noreply.github.com>
2024-12-23 17:42:52 -0500

    Update menu-toggle.js

--- assets/js/menu-toggle.js
+++ assets/js/menu-toggle.js
@@ -1,18 +1,25 @@
-// Toggle Mobile Menu
 document.addEventListener("DOMContentLoaded", () => {
   const menuToggle = document.getElementById("menu-toggle");
   const mobileMenu = document.getElementById("mobile-menu");
 
   if (menuToggle && mobileMenu) {
     menuToggle.addEventListener("click", () => {
-      const isHidden = mobileMenu.classList.contains("hidden");
-
-      // Toggle visibility
       mobileMenu.classList.toggle("hidden");
       mobileMenu.classList.toggle("active");
-
-      // Update aria-expanded for accessibility
-      menuToggle.setAttribute("aria-expanded", !isHidden);
     });
+  }
+  const modeToggleDesktop = document.getElementById("mode-toggle");
+  const modeToggleMobile = document.getElementById("mode-toggle-mobile");
+
+  const toggleDarkMode = () => {
+    document.body.classList.toggle("dark-mode");
+  };
+
+  if (modeToggleDesktop) {
+    modeToggleDesktop.addEventListener("click", toggleDarkMode);
+  }
+
+  if (modeToggleMobile) {
+    modeToggleMobile.addEventListener("click", toggleDarkMode);
   }
 });
