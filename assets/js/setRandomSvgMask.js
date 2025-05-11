9501a05748d1cd6f9735cd2b0dc57efbeb240c1d
benshindel <65996825+benshindel@users.noreply.github.com>
2025-05-10 23:39:57 -0400

    Update setRandomSvgMask.js

--- assets/js/setRandomSvgMask.js
+++ assets/js/setRandomSvgMask.js
@@ -5,14 +5,19 @@ document.addEventListener('DOMContentLoaded', function() {
     '/assets/images/svg/snail1.svg',
     '/assets/images/svg/okapi1.svg',
     '/assets/images/svg/lizard2.svg',
-    '/assets/images/svg/crow1.svg'
+    '/assets/images/svg/crow1.svg',
+    '/assets/images/svg/caterpillar1.svg',
+    '/assets/images/svg/caterpillar2.svg',
+    '/assets/images/svg/toad1.svg',
   ];
 
   const rightSvgFiles = [
     '/assets/images/svg/crow2.svg',
     '/assets/images/svg/pigeon1.svg',
     '/assets/images/svg/lizard1.svg',
-    '/assets/images/svg/lizard3.svg'
+    '/assets/images/svg/lizard3.svg',
+    '/assets/images/svg/crab1.svg',
+    '/assets/images/svg/crab2.svg'    
   ];
   
   /**
